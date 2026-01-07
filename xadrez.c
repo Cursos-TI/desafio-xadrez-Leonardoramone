162628d13762fa004be0fce2ccb339a6a030b219
Leonardoramone <152994797+Leonardoramone@users.noreply.github.com>
2026-01-07 14:37:51 +0000

    Movimentação do Bispo

--- xadrez.c
+++ xadrez.c
@@ -7,13 +7,18 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+   
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    printf("Movimentação do Bispo: \n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Cima, Direita\n");
+    }
+    
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
